237203a2054e578ad795062c8392a5dab148d4c4
SungHyun Choi <codeforchoi@gmail.com>
2024-02-06 00:08:36 +0900

    [Bronze IV] Title: 영수증, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,11 @@
+X = int(input())
+N = int(input())
+
+total = 0
+for _ in range(N):
+    price, num = map(int, input().split())
+    total += price * num
+if total == X:
+    print('Yes')
+else:
+    print('No')
