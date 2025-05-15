f25df1adefe0495c1e349fbc2d33b5fdcef94375
SungHyun Choi <codeforchoi@gmail.com>
2025-05-15 20:14:02 +0900

    [Bronze V] Title: 문자열, Time: 184 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        String[] strs = new String[3];
+        for (int i = 0; i < N; i++) {
+            strs[i] = sc.nextLine();
+        }
+
+        for (String str : strs) {
+            System.out.println("" + str.charAt(0) + str.charAt(str.length()-1));
+        }
+    }
+}
