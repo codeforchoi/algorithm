e414aaf88092749a4173482b02564528fc6666e2
SungHyun Choi <codeforchoi@gmail.com>
2025-05-23 02:24:24 +0900

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -14,11 +14,10 @@ public class Main {
         for (int i = 0; i < t; i++) {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
-            String str = st.nextToken();
-            String[] ch = str.split("");
+            String[] str = st.nextToken().split("");
 
-            for (int j = 0; j < ch.length; j++) {
-                sb.append(ch[j].repeat(r));
+            for (int j = 0; j < str.length; j++) {
+                sb.append(str[j].repeat(r));
             }
             if (i != t - 1) {
                 sb.append("\n");
