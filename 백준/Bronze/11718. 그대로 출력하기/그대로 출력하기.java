08e5dfddddc6217caaf2c628cf8f8a91265e6b78
SungHyun Choi <codeforchoi@gmail.com>
2025-05-24 18:41:23 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -5,15 +5,17 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         String str = "";
         int count = 0;
 
         while ((str = br.readLine()) != null) {
-            System.out.println(str);
+            sb.append(str.trim()).append("\n");
             count++;
             if (count > 100) {
                 break;
             }
         }
+        System.out.println(sb);
     }
 }
