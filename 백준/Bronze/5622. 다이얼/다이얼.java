c8d49b1c73b2143fce87e7ff91c39979f68d0af6
SungHyun Choi <codeforchoi@gmail.com>
2025-05-24 18:20:52 +0900

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -5,27 +5,19 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split("");
+        String[] strArr = {"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+        String inputStr = br.readLine();
         int totalTime = 0;
 
-        for (String s : str) {
-            char ch = s.charAt(0);
-            if (ch <= 'C') {
-                totalTime += 3;
-            } else if (ch <= 'F') {
-                totalTime += 4;
-            } else if (ch <= 'I') {
-                totalTime += 5;
-            } else if (ch <= 'L') {
-                totalTime += 6;
-            } else if (ch <= 'O') {
-                totalTime += 7;
-            } else if (ch <= 'S') {
-                totalTime += 8;
-            } else if (ch <= 'V') {
-                totalTime += 9;
-            } else {
-                totalTime += 10;
+        for (int i = 0; i < inputStr.length(); i++) {
+            char strToChar = inputStr.charAt(i);
+            String checkStr = String.valueOf(strToChar);
+
+            for (int j = 0; j < strArr.length; j++) {
+                if (strArr[j].contains(checkStr)) {
+                    totalTime += (j + 3);
+                    break;
+                }
             }
         }
         System.out.println(totalTime);
