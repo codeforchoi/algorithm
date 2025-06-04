68cc8e74c5e9bef1702d81ea7c4d2a950fa1ddf3
SungHyun Choi <codeforchoi@gmail.com>
2025-06-05 03:10:43 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,31 +1,24 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(reader.readLine());
         StringBuilder sb = new StringBuilder();
-        int[] cons = new int[6];
-        int x, y;
+        
+        int a = Integer.parseInt(token.nextToken());
+        int b = Integer.parseInt(token.nextToken());
+        int c = Integer.parseInt(token.nextToken());
+        int d = Integer.parseInt(token.nextToken());
+        int e = Integer.parseInt(token.nextToken());
+        int f = Integer.parseInt(token.nextToken());
 
-        for (int i = 0; i < 6; i++) {
-            cons[i] =Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = -999; i <= 999; i++) {
-            for (int j = -999; j <= 999; j++) {
-                int equation1 = cons[0] * i + cons[1] * j;
-                int equation2 = cons[3] * i + cons[4] * j;
-                if (equation1 == cons[2] && equation2 == cons[5]) {
-                    x = i;
-                    y = j;
-                    sb.append(x).append(" ").append(y);
-                }
-            }
-        }
+        int x = (c * e - b * f) / (a * e - b * d);
+        int y = (c * d - a * f) / (b * d - a * e);
+        
+        sb.append(x).append(" ").append(y);
         System.out.println(sb);
     }
 }
