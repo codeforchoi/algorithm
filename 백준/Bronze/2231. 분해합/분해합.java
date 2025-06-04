f841a0858c00e8ea41ecb565eeeeac6ea5e5d1f6
SungHyun Choi <codeforchoi@gmail.com>
2025-06-04 20:10:00 +0900

    [Bronze II] Title: 분해합, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -3,12 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    // n의 생성자는 처음부터 탐색할 필요없이 n의 각 자리수가 9로 최대일 경우를 뺀 수부터 탐색을 시작하면 된다.
+    // 따라서 n - (n_length * 9) 부터 탐색을 시작하면 불필요한 계산을 없앨 수 있다. 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        String number = br.readLine();
+        int n_length = number.length();
+        int n = Integer.parseInt(number);
         int result = 0;
         
-        for (int i = 0; i < n; i++) {
+        for (int i = n - (n_length * 9); i < n; i++) {
             int num = i;
             int sum = 0;
 
