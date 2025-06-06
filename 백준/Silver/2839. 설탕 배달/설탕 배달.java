fe3a131463192b8fd4b4d2f63c9e31f28e42e934
SungHyun Choi <codeforchoi@gmail.com>
2025-06-06 21:48:18 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,25 +1,21 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int result = 0;
 
-        while (true) {
-            if (n % 5 == 0) {
-                result += n / 5;
-                break;
-            }
-            n -= 3;
-            result++;
-            if (n < 0) {
-                result = -1;
-                break;
-            }
+        if (n == 4 || n == 7) { // n이 4, 7일 경우에는 예외
+            sb.append(-1);
+        } else if (n % 5 == 0) { // n이 5로 나누어질 경우
+            sb.append(n / 5);
+        } else if (n % 5 == 1 || n % 5 == 3) { // n이 6, 8, 11, 13, 16 등일 경우
+            sb.append((n / 5) + 1);
+        } else if (n % 5 == 2 || n % 5 == 4) { // n이 9, 12, 14, 17, 19 등일 경우
+            sb.append((n / 5) + 2);
         }
-        System.out.println(result);
+        System.out.println(sb);
     }
 }
