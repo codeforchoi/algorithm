4dbd4467609c406b11ca2988dff2d5321ad81c49
SungHyun Choi <codeforchoi@gmail.com>
2025-06-10 00:30:20 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        int strLength = str.length();
+        int[] arr = new int[strLength];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+        Arrays.sort(arr);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+        System.out.println(sb);
+    }
+}
