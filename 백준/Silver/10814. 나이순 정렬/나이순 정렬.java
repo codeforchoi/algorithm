2437ea0ae19449b1e1c954fe4adca99c9f41de86
SungHyun Choi <codeforchoi@gmail.com>
2025-06-11 02:46:21 +0900

    [Silver V] Title: 나이순 정렬, Time: 480 ms, Memory: 47608 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,45 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        Person[] person = new Person[n];
+
+        StringBuilder[] person = new StringBuilder[201];
+        for (int i = 0; i < person.length; i++) {
+            person[i] = new StringBuilder();
+        }
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            person[i] = new Person(age, name);
+            person[age].append(age + " " + name).append("\n");
         }
 
-        Arrays.sort(person, (p1, p2) -> p1.age - p2.age);
-
-        for (int i = 0; i < n; i++) {
-            sb.append(person[i]);
+        StringBuilder sb = new StringBuilder();
+        for (StringBuilder p : person) {
+            sb.append(p);
         }
         sb.setLength(sb.length() - 1);
         System.out.println(sb);
-    }
-
-    public static class Person {
-        int age;
-        String name;
-
-        public Person(int age, String name) {
-            this.age = age;
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return age + " " + name + "\n";
-        }
     }
 }
