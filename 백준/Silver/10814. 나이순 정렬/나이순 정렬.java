45c98e9ec0ea5262b6774e268d889cba4186be79
SungHyun Choi <codeforchoi@gmail.com>
2025-06-11 02:35:59 +0900

    [Silver V] Title: 나이순 정렬, Time: 588 ms, Memory: 50656 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -10,20 +10,36 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        String[][] person = new String[n][2];
+        Person[] person = new Person[n];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            person[i][0] = st.nextToken();
-            person[i][1] = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            person[i] = new Person(age, name);
         }
 
-        Arrays.sort(person, (p1, p2) -> Integer.parseInt(p1[0]) - Integer.parseInt(p2[0]));
+        Arrays.sort(person, (p1, p2) -> p1.age - p2.age);
 
         for (int i = 0; i < n; i++) {
-            sb.append(person[i][0] + " " + person[i][1]).append("\n");
+            sb.append(person[i]);
         }
         sb.setLength(sb.length() - 1);
         System.out.println(sb);
+    }
+
+    public static class Person {
+        int age;
+        String name;
+
+        public Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return age + " " + name + "\n";
+        }
     }
 }
