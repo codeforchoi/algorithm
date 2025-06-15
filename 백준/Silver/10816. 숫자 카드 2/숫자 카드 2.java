305d02cfca4c9b8875763d1fc8fc8e62d9df3f71
SungHyun Choi <codeforchoi@gmail.com>
2025-06-15 20:07:47 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 864 ms, Memory: 197528 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,22 +8,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> ownedCard = new HashMap<>();
+        int[] counting = new int[20000001]; // -10,000,000 ~ 10,000,000 입력범위
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            // getOrDefault(key, 0) 메서드
-            // key 키가 있으면 value값을 반환하고, 없으면 두번째 인자인 0을 반환
-            ownedCard.put(key, ownedCard.getOrDefault(key, 0) + 1);
+            counting[Integer.parseInt(st.nextToken()) + 10000000]++;
         }
 
         StringBuilder sb = new StringBuilder();
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            sb.append(ownedCard.getOrDefault(key, 0)).append(" ");
+            sb.append(counting[Integer.parseInt(st.nextToken()) + 10000000]).append(" ");
         }
         sb.setLength(sb.length() - 1);
         System.out.println(sb);
