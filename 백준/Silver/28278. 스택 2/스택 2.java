dbf7f691d4dedeec097be86f3eeedbe7aefa766c
SungHyun Choi <codeforchoi@gmail.com>
2025-06-22 00:05:07 +0900

    [Silver IV] Title: 스택 2, Time: 772 ms, Memory: 236256 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -21,28 +21,16 @@ public class Main {
                     stack.push(x);
                     break;
                 case 2:
-                    if (stack.isEmpty()) {
-                        sb.append(-1).append("\n");
-                    } else {
-                        sb.append(stack.pop()).append("\n");
-                    }
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
                     break;
                 case 3:
                     sb.append(stack.size()).append("\n");
                     break;
                 case 4:
-                    if (stack.isEmpty()) {
-                        sb.append(1).append("\n");
-                    } else {
-                        sb.append(0).append("\n");
-                    }
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
                     break;
                 case 5:
-                    if (stack.isEmpty()) {
-                        sb.append(-1).append("\n");
-                    } else {
-                        sb.append(stack.peek()).append("\n");
-                    }
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
                     break;
                 default:
                     break;
