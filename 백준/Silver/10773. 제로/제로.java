9a84b2a01764bc7141c5d0ce7f497d9e0e0a5a3c
SungHyun Choi <codeforchoi@gmail.com>
2025-06-22 01:06:00 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23140 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < k; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) stack.pop();
+            else stack.push(n);
+        }
+
+        int sum = 0;
+        int length = stack.size();
+        for (int i = 0; i < length; i++) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+    }
+}
