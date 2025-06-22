5c62c47cabf9e9f76e4400a983a67b2a9963a25d
SungHyun Choi <codeforchoi@gmail.com>
2025-06-23 01:36:34 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 172 ms, Memory: 19928 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String line = br.readLine();
+            if (line.equals(".")) break;
+            else sb.append(isVPS(line)).append("\n");
+        }
+        sb.setLength(sb.length() - 1);
+        System.out.println(sb);
+    }
+
+    public static String isVPS(String line) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : line.toCharArray()) {
+            switch(ch) {
+                case '(': case'[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if(stack.isEmpty()) return "no";
+                    else if (stack.peek() == '[') return "no";
+                    else if (stack.peek() == '(') stack.pop();
+                    break;
+                case ']':
+                    if(stack.isEmpty()) return "no";
+                    else if (stack.peek() == '(') return "no";
+                    else if (stack.peek() == '[') stack.pop();
+                default:
+                    break;
+            }
+        }
+        if (stack.isEmpty()) return "yes";
+        else return "no";
+    }
+}
