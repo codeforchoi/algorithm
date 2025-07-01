40d4d2acd876b761f8161e3db8df3ac67ca257b8
SungHyun Choi <codeforchoi@gmail.com>
2025-07-01 19:47:46 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int answer = 0;
+
+        if (n == 1) {
+            int num = Integer.parseInt(st.nextToken());
+            answer = num * num;
+        } else {
+            int min = 1000001;
+            int max = 0;
+            while (n-- > 0) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num < min) {
+                    min = num;
+                }
+
+                if (num > max) {
+                    max = num;
+                }
+            }
+            answer = min * max;
+        }
+        System.out.println(answer);
+    }
+}
