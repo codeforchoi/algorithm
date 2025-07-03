6edb1548fca28a1c8ec9912feeb994be25bec50c
SungHyun Choi <codeforchoi@gmail.com>
2025-07-03 21:52:27 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -6,12 +6,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(fibonacci(n));
-    }
+        int[] fibonacci = new int[n + 1];
 
-    public static int fibonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        for (int i = 0; i <= n; i++) {
+            if(i == 0) fibonacci[0] = 0;
+            else if(i == 1) fibonacci[1] = 1;
+            else fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+        System.out.println(fibonacci[n]);
     }
 }
