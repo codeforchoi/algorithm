163809a1bc46f83db340ab18934309fa1f501554
SungHyun Choi <codeforchoi@gmail.com>
2025-07-07 14:34:12 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(153).append("\n");
+        sb.append("codeforchoi");
+        System.out.println(sb);
+    }
+}
