a25c0ae2fdbad65ff2521e0e8bfcd8147e5b0267
SungHyun Choi <codeforchoi@gmail.com>
2025-07-11 22:26:14 +0900

    [Silver II] Title: 좌표 압축, Time: 1724 ms, Memory: 159324 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,7 +11,6 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         int[] sorted = new int[n];
-        HashMap<Integer, Integer> map = new HashMap<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
@@ -20,20 +18,35 @@ public class Main {
         }
         Arrays.sort(sorted);
 
-        // 정렬된 배열을 순회하면서 map에 없으면 넣고 count를 해준다.
-        int count = 0;
-        for (int num : sorted) {
-            if (!map.containsKey(num)) {
-                map.put(num, count);
-                count++;
+        // 인덱스가 곧 해당 요소보다 작은 요소의 개수가 되도록 한다.
+        int index = 0;
+        int prev = sorted[0];
+        for (int i = 1; i < n; i++) {
+            if (sorted[i] != prev) {
+                sorted[++index] = sorted[i];
+                prev = sorted[i];
             }
         }
 
-        // 원본 배열을 순회하면서 map에서 value값을 가져와 sb에 넣는다.
-        for (int key : arr) {
-            sb.append(map.get(key)).append(" ");
+        // 이진 탐색으로 0번 인덱스부터 설정한 index 값까지 탐색해 인덱스 값을 반환해준다.
+        for (int num : arr) {
+            sb.append(binarySearch(sorted, 0, index, num)).append(" ");
         }
         sb.setLength(sb.length() - 1);
         System.out.println(sb);
+    }
+
+    public static int binarySearch(int[] arr, int start, int end, int target) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
