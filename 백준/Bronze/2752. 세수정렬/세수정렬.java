f3318aee97fe4a191b7af3bb9e5793a21568ffe8
SungHyun Choi <codeforchoi@gmail.com>
2025-07-25 17:08:54 +0900

    [Bronze IV] Title: 세수정렬, Time: 116 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -8,11 +8,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int[] arr = new int[3];
         for (int i = 0; i < 3; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
+        for (int n : arr) {
+            sb.append(n).append(" ");
+        }
+        System.out.println(sb);
     }
 }
