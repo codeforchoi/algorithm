56c1f7d8f112bbcb23c79e7e6949bab5439a2ba1
SungHyun Choi <codeforchoi@gmail.com>
2025-08-04 22:30:09 +0900

    [Bronze IV] Title: !밀비 급일, Time: 112 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            StringBuilder line = new StringBuilder(br.readLine());
+            if (line.toString().equals("END")) {
+                break;
+            }
+            sb.append(line.reverse()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
