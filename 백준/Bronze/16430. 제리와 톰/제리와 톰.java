5c4c79066302228dc11a1ce1e7a173c26602cac8
SungHyun Choi <codeforchoi@gmail.com>
2025-08-25 14:54:23 +0900

    [Bronze V] Title: 제리와 톰, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/16430. 제리와 톰/제리와 톰.java
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        System.out.println(b - a + " " + b);
+        sb.append(b - a).append(" ").append(b);
+        System.out.println(sb);
     }
 }
