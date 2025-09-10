bd788a3a591281ae4d630f0328bf619f1c020376
SungHyun Choi <codeforchoi@gmail.com>
2025-09-10 12:45:51 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        sb.append(k / m).append(" ").append(k % m);
+        System.out.println(sb);
+    }
+}
