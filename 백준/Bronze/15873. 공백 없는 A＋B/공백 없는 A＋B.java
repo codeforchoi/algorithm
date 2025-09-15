32f4f4d8d03c2afd8c50437e7d16f7e85d9a65da
SungHyun Choi <codeforchoi@gmail.com>
2025-09-15 14:27:03 +0900

    [Bronze IV] Title: 공백 없는 A+B, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/15873. 공백 없는 A＋B/공백 없는 A＋B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if (str.charAt(1) == '0') {
+            System.out.println(10 + Integer.parseInt(str.substring(2)));
+        } else {
+            System.out.println(str.charAt(0) - '0' + Integer.parseInt(str.substring(1)));
+        }
+    }
+}
