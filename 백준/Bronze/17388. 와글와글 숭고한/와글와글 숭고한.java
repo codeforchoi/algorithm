1fa81d0d1d7fdc8360cbd90738998784b42917bb
SungHyun Choi <codeforchoi@gmail.com>
2025-09-19 21:06:07 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int min = Math.min(s, Math.min(k, h));
+        if (s + k + h < 100) {
+            if (min == s) {
+                sb.append("Soongsil");
+            } else if (min == k) {
+                sb.append("Korea");
+            } else {
+                sb.append("Hanyang");
+            }
+        } else {
+            sb.append("OK");
+        }
+        System.out.println(sb);
+    }
+}
