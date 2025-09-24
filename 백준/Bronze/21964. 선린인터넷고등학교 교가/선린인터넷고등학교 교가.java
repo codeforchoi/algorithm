5def022393e094d6f3b0ce9d73b1964a66831c99
SungHyun Choi <codeforchoi@gmail.com>
2025-09-24 17:53:29 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 116 ms, Memory: 15344 KB -BaekjoonHub

--- 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -5,8 +5,10 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         String s = br.readLine();
-        System.out.println(s.substring(n - 5));
+        sb.append(s.substring(n - 5));
+        System.out.println(sb);
     }
 }
