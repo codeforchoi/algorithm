6b120f93d15b5c69a35c8e78657fff7a9ae7ec08
SungHyun Choi <codeforchoi@gmail.com>
2025-09-30 13:10:09 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 100 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.java
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.java
@@ -9,22 +9,12 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        while(t-- > 0) {
+        for (int i = 0; i < t; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            int count = 0;
-            for (int i = 1; i <= a; i++) {
-                for (int j = 1; j <= b; j++) {
-                    for (int k = 1; k <= c; k++) {
-                        if ((i % j == j % k) && (j % k == k % i) && (i % j == k % i)) {
-                            count++;
-                        }
-                    }
-                }
-            }
-            sb.append(count).append("\n");
+            sb.append(Math.min(a, Math.min(b, c))).append("\n");
         }
         System.out.println(sb);
     }
