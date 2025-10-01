c986fac865dfee9535482986910b2f7d174bb976
SungHyun Choi <codeforchoi@gmail.com>
2025-10-01 12:39:39 +0900

    [Bronze IV] Title: 푸앙이와 종윤이, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.java
+++ 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.java
@@ -8,14 +8,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = 100 - Integer.parseInt(st.nextToken());
-        int b = 100 - Integer.parseInt(st.nextToken());
-        int c = 100 - (a + b);
-        int d =  a * b;
-        int q = d / 100;
-        int r = d % 100;
-        sb.append(a).append(" ").append(b).append(" ").append(c).append(" ").append(d).append(" ").append(q).append(" ").append(r).append("\n");
-        sb.append(c + q).append(" ").append(r);
+        int[] arr = new int[6];
+        arr[0] = 100 - Integer.parseInt(st.nextToken());
+        arr[1] = 100 - Integer.parseInt(st.nextToken());
+        arr[2] = 100 - (arr[0] + arr[1]);
+        arr[3] =  arr[0] * arr[1];
+        arr[4] = arr[3] / 100;
+        arr[5] = arr[3] % 100;
+        for (int i = 0; i < 6; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        sb.append("\n");
+        sb.append(arr[2] + arr[4]).append(" ").append(arr[5]);
         System.out.println(sb);
     }
 }
