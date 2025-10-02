2fa31dfc53d152336a90bb639e28993d2222c95a
SungHyun Choi <codeforchoi@gmail.com>
2025-10-02 14:36:44 +0900

    [Bronze IV] Title: Triathlon, Time: 120 ms, Memory: 14892 KB -BaekjoonHub

---
+++ 백준/Bronze/25600. Triathlon/Triathlon.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int maxScore = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int score = 0;
+            int a = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            if (a == d + g) {
+                score = 2 * a * a;
+            } else {
+                score = a * (d + g);
+            }
+            if (score > maxScore) {
+                maxScore = score;
+            }
+        }
+        System.out.println(maxScore);
+    }
+}
