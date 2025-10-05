2e2d324bc2b29e6ff2b538a5d0a94b2951cde611
SungHyun Choi <codeforchoi@gmail.com>
2025-10-05 23:51:02 +0900

    [Bronze IV] Title: MBTI, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/25640. MBTI/MBTI.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String jinho = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+        while (n-- > 0) {
+            String mbti = br.readLine();
+            if (jinho.equals(mbti)) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
