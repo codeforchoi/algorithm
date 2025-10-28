c6550043f65db5e6b640dc26b13a54b44470f2b8
SungHyun Choi <codeforchoi@gmail.com>
2025-10-28 13:29:03 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.java
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.java
@@ -5,36 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] lines = new String[29];
         for (int i = 0; i < 15; i++) {
-            lines[i] = br.readLine();
-        }
-
-        boolean flag = false;
-        for (int i = 0; i < 15; i++) {
-            String line = lines[i];
-            for (int j = 0; j < 29; j++) {
-                char ch = line.charAt(j);
-                switch(ch) {
-                    case 'w':
-                        flag = true;
-                        System.out.println("chunbae");
-                        break;
-                    case 'b':
-                        flag = true;
-                        System.out.println("nabi");
-                        break;
-                    case 'g':
-                        flag = true;
-                        System.out.println("yeongcheol");
-                        break;
-                    default:
-                }
-                if (flag) {
-                    break;
-                }
-            }
-            if (flag) {
+            String str = br.readLine();
+            if (str.contains("w")) {
+                System.out.println("chunbae");
+                break;
+            } else if (str.contains("b")) {
+                System.out.println("nabi");
+                break;
+            } else if (str.contains("g")) {
+                System.out.println("yeongcheol");
                 break;
             }
         }
