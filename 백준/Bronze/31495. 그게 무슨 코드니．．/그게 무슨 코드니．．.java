feba0c65429126d8a2944038aae717ea3cb04cbe
SungHyun Choi <codeforchoi@gmail.com>
2025-11-05 02:00:13 +0900

    [Bronze IV] Title: 그게 무슨 코드니.., Time: 100 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if (str.length() <= 2) {
+            System.out.println("CE");
+        } else if (str.charAt(0) == '"' && str.charAt(str.length() - 1) == '"') {
+            System.out.println(str.substring(1, str.length() - 1));
+        } else {
+            System.out.println("CE");
+        }
+    }
+}
