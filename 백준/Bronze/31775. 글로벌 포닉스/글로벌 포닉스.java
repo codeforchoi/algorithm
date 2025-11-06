0232a5177de63fbcf6082dc6c2822cb9a312ae5e
SungHyun Choi <codeforchoi@gmail.com>
2025-11-06 16:18:32 +0900

    [Bronze IV] Title: 글로벌 포닉스, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/31775. 글로벌 포닉스/글로벌 포닉스.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean isL = false;
+        boolean isK = false;
+        boolean isP = false;
+        for (int i = 0; i < 3; i++) {
+            char ch = br.readLine().charAt(0);
+            if (ch == 'l') {
+                isL = true;
+            } else if (ch == 'k') {
+                isK = true;
+            } else if (ch == 'p') {
+                isP = true;
+            }
+        }
+        System.out.println(isL && isK && isP ? "GLOBAL" : "PONIX");
+    }
+}
