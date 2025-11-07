d50acd1ee1aab23f9f14e5a288c3f72538b41957
SungHyun Choi <codeforchoi@gmail.com>
2025-11-07 15:11:37 +0900

    [Bronze IV] Title: 학식 사주기, Time: 100 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/31821. 학식 사주기/학식 사주기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] price = new int[n];
+        for (int i = 0; i < n; i++) {
+            price[i] = Integer.parseInt(br.readLine());
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        int totalPrice = 0;
+        for (int i = 0; i < m; i++) {
+            totalPrice += price[Integer.parseInt(br.readLine()) - 1];
+        }
+        System.out.println(totalPrice);
+    }
+}
