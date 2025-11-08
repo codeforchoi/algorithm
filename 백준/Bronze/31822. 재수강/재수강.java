0d3f2e51a7aca07f2277db1951a9e7ce9bc820fb
SungHyun Choi <codeforchoi@gmail.com>
2025-11-08 16:12:12 +0900

    [Bronze IV] Title: 재수강, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/31822. 재수강/재수강.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String target = br.readLine().substring(0, 5);
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        while (n-- > 0) {
+            String code = br.readLine().substring(0, 5);
+            if (target.equals(code)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
