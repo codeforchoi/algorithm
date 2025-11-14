412dc53fc4f6a231b79d95973e41f87e877d8143
SungHyun Choi <codeforchoi@gmail.com>
2025-11-14 17:41:12 +0900

    [Bronze IV] Title: 바코드 닉네임, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/32288. 바코드 닉네임/바코드 닉네임.java
+++ 백준/Bronze/32288. 바코드 닉네임/바코드 닉네임.java
@@ -5,8 +5,12 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        String str = br.readLine().replaceAll("I", "i").replaceAll("l", "L");
-        System.out.println(str);
+        String str = br.readLine();
+        for (int i = 0; i < n; i++) {
+            sb.append(str.charAt(i) == 'I' ? "i" : "L");
+        }
+        System.out.println(sb);
     }
 }
