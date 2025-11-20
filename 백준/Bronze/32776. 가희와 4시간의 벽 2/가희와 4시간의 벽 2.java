90ece2c73f452501b427bac749ac9643c1a6933e
SungHyun Choi <codeforchoi@gmail.com>
2025-11-20 17:21:56 +0900

    [Bronze IV] Title: 가희와 4시간의 벽 2, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/32776. 가희와 4시간의 벽 2/가희와 4시간의 벽 2.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int s = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int flight = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+        System.out.println(s <= 240 || s <= flight ? "high speed rail" : "flight");
+    }
+}
