c40fc3137c58d0c1c3ab82c5e0e81983167a2c9c
SungHyun Choi <codeforchoi@gmail.com>
2025-11-21 21:50:50 +0900

    [Bronze IV] Title: 왜 맘대로 예약하냐고, Time: 124 ms, Memory: 14788 KB -BaekjoonHub

---
+++ 백준/Bronze/32941. 왜 맘대로 예약하냐고/왜 맘대로 예약하냐고.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        boolean[] flag = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            int k = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < k; j++) {
+                if (Integer.parseInt(st.nextToken()) == x) {
+                    flag[i] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (flag[i] == false) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
+}
