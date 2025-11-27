a685ae0ba49256fa662682f0d04886cdfcaabde0
SungHyun Choi <codeforchoi@gmail.com>
2025-11-27 16:46:59 +0900

    [Bronze IV] Title: 첫 번째 문제는 정말 쉬운 문제일까?, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/34071. 첫 번째 문제는 정말 쉬운 문제일까？/첫 번째 문제는 정말 쉬운 문제일까？.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int first = arr[0];
+        Arrays.sort(arr);
+        System.out.println(arr[0] == first ? "ez" : arr[n - 1] == first ? "hard" : "?");
+    }
+}
