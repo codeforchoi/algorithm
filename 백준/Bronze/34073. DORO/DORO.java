9b3f70f07c3317089761e6908e7e38685b30213f
SungHyun Choi <codeforchoi@gmail.com>
2025-11-28 23:41:19 +0900

    [Bronze IV] Title: DORO, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            sb.append(st.nextToken()).append("DORO").append(" ");
+        }
+        sb.setLength(sb.length() - 1);
+        System.out.println(sb);
+    }
+}
