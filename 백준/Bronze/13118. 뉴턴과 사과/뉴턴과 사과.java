ceb5e6c19c880d139bac0dbe61cb81e106a5fe43
SungHyun Choi <codeforchoi@gmail.com>
2025-12-08 14:24:21 +0900

    [Bronze IV] Title: 뉴턴과 사과, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/13118. 뉴턴과 사과/뉴턴과 사과.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] p = new int[4];
+        for (int i = 0; i < 4; i++) {
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int index = 0;
+        for (int i = 0; i < 4; i++) {
+            if (p[i] == x) {
+                index = i + 1;
+            }
+        }
+        System.out.println(Math.max(index, 0));
+    }
+}
