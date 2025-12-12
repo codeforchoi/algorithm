2e96a40b8a866ff51e904ed322567436af2cd064
SungHyun Choi <codeforchoi@gmail.com>
2025-12-12 21:09:26 +0900

    [Bronze IV] Title: 연대 다음 고대, Time: 112 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/34824. 연대 다음 고대/연대 다음 고대.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        boolean yonsei = false;
+        boolean korea = false;
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            if (str.equals("yonsei") && !korea) {
+                yonsei = true;
+                sb.append("Yonsei Won!");
+            } else if (str.equals("korea") && !yonsei) {
+                korea = true;
+                sb.append("Yonsei Lost...");
+            }
+        }
+        System.out.println(sb);
+    }
+}
