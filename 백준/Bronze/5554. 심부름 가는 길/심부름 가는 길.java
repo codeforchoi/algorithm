4bddca35d41bfe5ccd988360ffc65f21371372a0
SungHyun Choi <codeforchoi@gmail.com>
2025-12-16 14:22:37 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int totalSec = 0;
+        for (int i = 0; i < 4; i++) {
+            totalSec += Integer.parseInt(br.readLine());
+        }
+        sb.append(totalSec / 60).append("\n").append(totalSec % 60);
+        System.out.println(sb);
+    }
+}
