07af009c2e1cfc5cca961bf985863d6b2e033ab3
SungHyun Choi <codeforchoi@gmail.com>
2025-12-25 00:30:14 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 124 ms, Memory: 15992 KB -BaekjoonHub

---
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = Integer.parseInt(st.nextToken());
+        int diff = Integer.parseInt(st.nextToken());
+        int a = (sum + diff) / 2;
+        int b = sum - a;
+        if ((sum + diff) % 2 != 0 || sum < diff) {
+            System.out.println(-1);
+        } else {
+            System.out.println(Math.max(a, b) + " " + Math.min(a, b));
+        }
+    }
+}
