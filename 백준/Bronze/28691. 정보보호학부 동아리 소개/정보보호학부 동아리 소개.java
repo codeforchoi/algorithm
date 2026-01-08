3b8c1c694f9728bd932b14e3057d21b0a6c20446
SungHyun Choi <codeforchoi@gmail.com>
2026-01-08 13:12:24 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 96 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char ch = br.readLine().charAt(0);
+        switch(ch) {
+            case 'M':
+                System.out.println("MatKor");
+                break;
+            case 'W' :
+                System.out.println("WiCys");
+                break;
+            case 'C':
+                System.out.println("CyKor");
+                break;
+            case 'A':
+                System.out.println("AlKor");
+                break;
+            case'$':
+                System.out.println("$clear");
+                break;
+            default:
+                break;
+        }
+    }
+}
