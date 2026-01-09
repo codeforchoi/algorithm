a2481c7b46ebb59dd86079a34ad36164cbf74502
SungHyun Choi <codeforchoi@gmail.com>
2026-01-10 01:10:58 +0900

    [Bronze V] Title: 세제곱의 합, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int sum = n * (n + 1) / 2;
+        sb.append(sum).append("\n");
+        sb.append((long)Math.pow(sum, 2)).append("\n");
+        sb.append((long)Math.pow(sum, 2));
+        System.out.println(sb);
+    }
+}
