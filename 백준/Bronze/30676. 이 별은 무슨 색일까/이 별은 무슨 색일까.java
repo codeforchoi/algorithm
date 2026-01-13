b09f61900b9b21b6409803d49866b0acd56eb758
SungHyun Choi <codeforchoi@gmail.com>
2026-01-14 02:57:20 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int lamda = Integer.parseInt(br.readLine());
+        if (lamda >= 620) {
+            System.out.println("Red");
+        } else if (lamda >= 590) {
+            System.out.println("Orange");
+        } else if (lamda >= 570) {
+            System.out.println("Yellow");
+        } else if (lamda >= 495) {
+            System.out.println("Green");
+        } else if (lamda >= 450) {
+            System.out.println("Blue");
+        } else if (lamda >= 425) {
+            System.out.println("Indigo");
+        } else {
+            System.out.println("Violet");
+        }
+    }
+}
