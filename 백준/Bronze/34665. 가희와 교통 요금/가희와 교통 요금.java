33beb32c3f14c52a3a85f978e68e874a9ec67ee4
SungHyun Choi <codeforchoi@gmail.com>
2026-01-26 14:27:45 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        System.out.println(a.equals(b) ? 0 : 1550);
+    }
+}
