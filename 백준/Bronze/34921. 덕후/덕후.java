ab3862a6680a9bd04cc3d12cf27e587de54ba367
SungHyun Choi <codeforchoi@gmail.com>
2026-01-29 21:42:37 +0900

    [Bronze V] Title: 덕후, Time: 92 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/34921. 덕후/덕후.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+        System.out.println(Math.max(10 + 2 * (25 - a + t), 0));
+    }
+}
