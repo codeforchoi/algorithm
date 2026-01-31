aeac1fac2827f59c884d388f7b81455221e56256
SungHyun Choi <codeforchoi@gmail.com>
2026-01-31 13:35:35 +0900

    [Bronze V] Title: 공통교육과정, Time: 92 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char code = br.readLine().charAt(0);
+        switch(code) {
+            case 'F':
+                System.out.println("Foundation");
+                break;
+            case 'C':
+                System.out.println("Claves");
+                break;
+            case 'V':
+                System.out.println("Veritas");
+                break;
+            case 'E':
+                System.out.println("Exploration");
+                break;
+            default:
+        }
+    }
+}
