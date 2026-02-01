ca13f0dee4d10a13ce4f71e4cb3540e5d16c848d
SungHyun Choi <codeforchoi@gmail.com>
2026-02-01 12:48:00 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        sb.append("I love DGU\n".repeat(n));
+        sb.setLength(sb.length() - 1);
+        System.out.println(sb);
+    }
+}
