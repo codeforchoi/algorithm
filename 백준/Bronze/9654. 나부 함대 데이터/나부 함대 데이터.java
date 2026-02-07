ddb0fbab790a36cce78dc8023b4d1590911b708a
SungHyun Choi <codeforchoi@gmail.com>
2026-02-07 16:36:22 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 88 ms, Memory: 13932 KB -BaekjoonHub

--- 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        sb.append("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n");
-        sb.append("N2 Bomber      Heavy Fighter  Limited    21        \n");
-        sb.append("J-Type 327     Light Combat   Unlimited  1         \n");
-        sb.append("NX Cruiser     Medium Fighter Limited    18        \n");
-        sb.append("N1 Starfighter Medium Fighter Unlimited  25        \n");
-        sb.append("Royal Cruiser  Light Combat   Limited    4         \n");
+        sb.append("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n")
+                .append("N2 Bomber      Heavy Fighter  Limited    21        \n")
+                .append("J-Type 327     Light Combat   Unlimited  1         \n")
+                .append("NX Cruiser     Medium Fighter Limited    18        \n")
+                .append("N1 Starfighter Medium Fighter Unlimited  25        \n")
+                .append("Royal Cruiser  Light Combat   Limited    4         ");
         System.out.println(sb);
     }
 }
