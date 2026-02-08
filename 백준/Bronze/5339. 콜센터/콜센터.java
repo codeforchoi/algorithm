aa54180b69b0f8f6c1b510141a99fb23939df5c9
SungHyun Choi <codeforchoi@gmail.com>
2026-02-08 17:03:34 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("     /~\\\n")
+                .append("    ( oo|\n")
+                .append("    _\\=/_\n")
+                .append("   /  _  \\\n")
+                .append("  //|/.\\|\\\\\n")
+                .append(" ||  \\ /  ||\n")
+                .append("============\n")
+                .append("|          |\n".repeat(3));
+        sb.setLength(sb.length() - 1);
+        System.out.println(sb);
+    }
+}
