d8ef9b1e839cdfd1034d31ad3ec4d53f5b4a5e7f
SungHyun Choi <codeforchoi@gmail.com>
2026-02-09 15:14:56 +0900

    [Bronze V] Title: Hook, Time: 88 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#  # #### #### #  #\n")
+                .append("#### #  # #  # # #\n")
+                .append("#### #  # #  # # #\n")
+                .append("#  # #### #### #  #\n");
+        System.out.println(sb);
+    }
+}
