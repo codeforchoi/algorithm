91402b76ae6500796d2c3cd64c27938f8ef16675
SungHyun Choi <codeforchoi@gmail.com>
2026-02-21 12:59:49 +0900

    [level 0] Title: 카운트 업, Time: 2.07 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,10 +1,7 @@
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 class Solution {
     public static int[] solution(int start_num, int end_num) {
-        return start_num <= end_num
-                ? IntStream.rangeClosed(start_num, end_num).toArray()
-                : IntStream.rangeClosed(end_num, start_num).toArray();
+        return IntStream.rangeClosed(start_num, end_num).toArray();
     }
 }
