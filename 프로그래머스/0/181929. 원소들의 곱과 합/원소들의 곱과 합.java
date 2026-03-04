bc1af68dccdc56469e1a8543fedcbeb1ff39460e
SungHyun Choi <codeforchoi@gmail.com>
2026-03-04 13:55:00 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 1.42 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list) {
+        return Arrays.stream(num_list).reduce(1, (a, b) -> a * b) < Math.pow(Arrays.stream(num_list).sum(), 2) ? 1 : 0;
+    }
+}
