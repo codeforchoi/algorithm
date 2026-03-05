e36e8d8354cfd0de07cc1455602637e5b3dc0566
SungHyun Choi <codeforchoi@gmail.com>
2026-03-06 01:51:49 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.35 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,18 +1,5 @@
 class Solution {
     public int solution(int n, String control) {
-        for (String s : control.split("")) {
-            switch(s) {
-                case"w":
-                    n++; break;
-                case "s":
-                    n--; break;
-                case "d":
-                    n += 10; break;
-                case "a":
-                    n -= 10; break;
-                default:
-            }
-        }
-        return n;
+        return control.chars().reduce(n, (temp, c) -> temp + (c == 'w' ? 1 : c == 's' ? -1 : c == 'd' ? 10 : -10));
     }
 }
