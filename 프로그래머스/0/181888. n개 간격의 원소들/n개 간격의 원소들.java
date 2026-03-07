051cb9f33af2764504d8654ee01f17f0b5baa7e3
SungHyun Choi <codeforchoi@gmail.com>
2026-03-07 14:42:29 +0900

    [level 0] Title: n개 간격의 원소들, Time: 2.79 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < num_list.length; i += n) {
+            list.add(num_list[i]);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
