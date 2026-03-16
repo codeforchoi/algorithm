ffd682ec9564ec9de336b8eb41c3108542252021
SungHyun Choi <codeforchoi@gmail.com>
2026-03-17 02:44:47 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 11.78 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        return IntStream.range(2, n)
+                .filter(x -> n % x == 1)
+                .findFirst().orElse(0);
+    }
+}
