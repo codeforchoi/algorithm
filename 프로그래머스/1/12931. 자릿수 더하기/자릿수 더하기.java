3d36917c40fa1abe50a1f961ee4bf6e81457dac8
SungHyun Choi <codeforchoi@gmail.com>
2026-03-22 13:33:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.48 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,8 +1,5 @@
-import java.util.Arrays;
-
 public class Solution {
     public int solution(int n) {
-        String[] arr = String.valueOf(n).split("");
-        return Arrays.stream(arr).mapToInt(Integer::parseInt).sum();
+        return String.valueOf(n).chars().map(c -> c - '0').sum();
     }
 }
