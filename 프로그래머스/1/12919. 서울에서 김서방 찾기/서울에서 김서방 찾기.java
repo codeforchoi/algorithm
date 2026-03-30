f85383981681ef47734a687313f47846752ede94
SungHyun Choi <codeforchoi@gmail.com>
2026-03-30 14:07:30 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.13 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,8 +2,9 @@ class Solution {
     public String solution(String[] seoul) {
         StringBuilder sb = new StringBuilder();
         int index = 0;
-        for (int i = 0; i < seoul.length; i++) {
-            if(seoul[i].equals("Kim")) index = i;
+        for (String s : seoul) {
+            if(s.equals("Kim")) break;
+            index++;
         }
         sb.append("김서방은 ").append(index).append("에 있다");
         return sb.toString();
