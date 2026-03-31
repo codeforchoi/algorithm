8ffcc67ba2ab9624801dadb55e650e40958d420c
SungHyun Choi <codeforchoi@gmail.com>
2026-03-31 15:34:41 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int num) {
-        if (num == 1) return 0;
-
         long n = num;
-        int count = 0;
-        while (count < 501) {
-            if(n == 1) break;
-            n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
-            count++;
+        for (int i = 0; i < 500; i++) {
+            if(n == 1) return i;
+            n = (n % 2 ==0) ? n / 2 : n * 3 + 1;
         }
-        if(count > 500) count = -1;
-        return count;
+        return -1;
     }
 }
