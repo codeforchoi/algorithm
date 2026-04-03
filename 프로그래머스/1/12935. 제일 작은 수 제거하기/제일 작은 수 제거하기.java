acd60875c0406a5b612b19944a2d7dc40db09260
SungHyun Choi <codeforchoi@gmail.com>
2026-04-04 02:40:47 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 14.52 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,24 +1,11 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] arr) {
         if (arr.length == 1) {
             return new int[]{-1};
         }
-
-        int min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            min = Math.min(min, arr[i]);
-        }
-
-        int index = 0;
-        int[] result = new int[arr.length - 1];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == min) {
-                continue;
-            } else {
-                result[index] = arr[i];
-                index++;
-            }
-        }
-        return result;
+        int min = Arrays.stream(arr).min().getAsInt();
+        return Arrays.stream(arr).filter(i -> i != min).toArray();
     }
 }
