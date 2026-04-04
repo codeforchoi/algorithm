06dbd365f1b64e3336dbf14ef9f689705e5d8470
SungHyun Choi <codeforchoi@gmail.com>
2026-04-05 00:17:08 +0900

    [level 1] Title: 내적, Time: 3.88 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,9 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] a, int[] b) {
-        int result = 0;
-        for (int i = 0; i < a.length; i++) {
-            result += a[i] * b[i];
-        }
-        return result;
+        return IntStream.range(0, a.length).map(i -> a[i] * b[i]).sum();
     }
 }
