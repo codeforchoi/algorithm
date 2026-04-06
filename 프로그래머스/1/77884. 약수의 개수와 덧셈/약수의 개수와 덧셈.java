0226d66d93604184433aca124829221d0760b7a2
SungHyun Choi <codeforchoi@gmail.com>
2026-04-06 12:35:08 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,14 +1,9 @@
 class Solution {
     public int solution(int left, int right) {
         int sum = 0;
+        // 제곱수인 경우 약수의 개수가 홀수
         for (int i = left; i <= right; i++) {
-            int count = 1;
-            for (int j = 1; j <= i / 2; j++) {
-                if (i % j == 0) {
-                    count++;
-                }
-            }
-            sum += i * ((count % 2 == 0) ? 1 : -1);
+            sum += (i % Math.sqrt(i) == 0) ? -i : i;
         }
         return sum;
     }
