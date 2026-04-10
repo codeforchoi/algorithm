699a52f8b79fb9f129e0947a1e8f28ef2db0e3f5
SungHyun Choi <codeforchoi@gmail.com>
2026-04-10 16:35:58 +0900

    [level 1] Title: 행렬의 덧셈, Time: 3.61 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int n = arr1.length;
-        int m = arr1[0].length;
-        int[][] result = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                result[i][j] = arr1[i][j] + arr2[i][j];
+        int[][] result = arr1;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                result[i][j] += arr2[i][j];
             }
         }
         return result;
