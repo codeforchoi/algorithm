ba98699c6a689ba3bb814f98e1ee6a523de7c10a
SungHyun Choi <codeforchoi@gmail.com>
2026-04-22 16:39:09 +0900

    [Gold IV] Title: 팰린드롬?, Time: 708 ms, Memory: 218396 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,38 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] numbers;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
-        numbers = new int[N];
+        int[] numbers = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             numbers[i] = Integer.parseInt(st.nextToken());
         }
 
+        boolean[][] dp = new boolean[N][N];
+
+        // 길이가 1인 경우
+        for (int i = 0; i < N; i++) {
+            dp[i][i] = true;
+        }
+
+        // 길이가 2인 경우
+        for (int i = 0; i < N - 1; i++) {
+            if (numbers[i] == numbers[i + 1]) {
+                dp[i][i + 1]  = true;
+            }
+        }
+        
+        // 길이가 3이상인 경우
+        for (int len = 3; len <= N; len++) {
+            for (int i = 0; i <= N - len; i++) {
+                int j = i + len - 1;
+
+                // 양 끝 값이 같고, 그 내부가 팰린드롬이면 팰린드롬이다. (dp[i + 1][j - 1]는 내부에 이미 계산된 값)
+                if (numbers[i] == numbers[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
         int M = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken()) - 1; // 번째 수이므로 인덱스에 맞게 바꾼다.
-            int end = Integer.parseInt(st.nextToken()) - 1;
-            sb.append(isPalindrome(start, end) ? 1 : 0).append("\n");
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int e = Integer.parseInt(st.nextToken()) - 1;
+            sb.append(dp[s][e] ? 1 : 0).append("\n");
         }
         sb.setLength(sb.length() - 1);
         System.out.println(sb);
-    }
-
-    private static boolean isPalindrome(int start, int end) {
-        int diff = end - start;
-        int count = (diff % 2 == 0) ? diff / 2 : (diff + 1) / 2;
-        for (int i = 0; i < count; i++) {
-            if (numbers[start + i] != numbers[end - i]) {
-                return false;
-            }
-        }
-        return true;
     }
 }
