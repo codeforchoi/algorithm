813443090a20148f13f48739a85acf79b22bd31c
SungHyun Choi <codeforchoi@gmail.com>
2026-04-28 00:31:58 +0900

    [Platinum V] Title: 약 팔기, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        StringBuilder sb = new StringBuilder();        
+        sb.append(2000).append("\n");        
+        for (int i = 0; i < 1000; i++) sb.append(1).append(" ");
+        for (int i = 0; i < 1000; i++) sb.append(1000).append(" ");
+        System.out.println(sb);
+    }
+}
