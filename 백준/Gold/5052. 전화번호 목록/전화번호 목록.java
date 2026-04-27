b6ee3bb8b2b58a8d59dcb16eeb9efeb2e00cd223
SungHyun Choi <codeforchoi@gmail.com>
2026-04-27 22:20:40 +0900

    [Gold IV] Title: 전화번호 목록, Time: 540 ms, Memory: 33004 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -4,20 +4,19 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+        int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine()); // 전화번호 개수
+            int n = Integer.parseInt(br.readLine());
             String[] phoneNumbers = new String[n];
 
             for (int i = 0; i < n; i++) {
                 phoneNumbers[i] = br.readLine();
             }
 
-            // 1. 사전순으로 정렬
             Arrays.sort(phoneNumbers);
 
-            // 2. 인접한 두 번호만 비교
+            // 인접한 두 번호만 비교
             boolean isConsistent = true;
             for (int i = 0; i < n - 1; i++) {
                 // 뒷 번호가 앞 번호로 시작하는지 확인
@@ -27,7 +26,6 @@ public class Main {
                 }
             }
 
-            // 3. 결과 출력
             if (isConsistent) {
                 System.out.println("YES");
             } else {
