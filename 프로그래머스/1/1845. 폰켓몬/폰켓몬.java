6ffada7f7145fac90918f874baaebbade0af6773
SungHyun Choi <codeforchoi@gmail.com>
2026-07-31 09:18:20 +0900

    [level 1] Title: 폰켓몬, Time: 5.33 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,21 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int n = nums.length / 2;
-		int maxCount = 0;
+        int n = nums.length / 2;		
 		Map<Integer, Integer> phoneketmonCount = new HashMap<>();
 		
 		for(int num : nums) {
 			phoneketmonCount.put(num, phoneketmonCount.getOrDefault(num, 0) + 1);
-		}
+		}	
 		
-		for(int key : phoneketmonCount.keySet()) {
-			if(maxCount >= n) {
-				break;
-			}
-			maxCount++;
-		}
-		
-		return maxCount;
+		return Math.min(n, phoneketmonCount.size());
     }
 }
