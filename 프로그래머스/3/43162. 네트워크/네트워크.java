07f9369921eced0718ebf524a8b67dc88b8f294c
SungHyun Choi <codeforchoi@gmail.com>
2026-08-07 15:42:06 +0900

    [level 3] Title: 네트워크, Time: 1.15 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,49 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
-        int answer = 0;
+    private static int[] parent;
 
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                dfs(i, computers, visited);
-                answer++;
-            }
-        }
-        return answer;
+	public static int solution(int n, int[][] computers) {
+		List<Integer>[] graph = new ArrayList[n];
+		for(int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(i != j && computers[i][j] == 1) {
+					graph[i].add(j);	
+				}
+			}
+		}
+		
+		parent = new int[n];
+		for(int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+		
+		int count = 0;
+		for(int i = 0; i < n; i++) {
+			for(int v : graph[i]) {
+				if(count == n - 1) break;
+				if(find(i) != find(v)) {
+					union(i, v);
+					count++;
+				}
+			}
+		}
+		
+        return n - count;
     }
-    
-    private static void dfs(int cur, int[][] computers, boolean[] vistied) {
-        vistied[cur] = true;
-
-        for(int i = 0; i < computers.length; i++) {
-            if(computers[cur][i] == 1 && !vistied[i]) {
-                dfs(i, computers, vistied);
-            }
-        }
-    }
+	
+	private static int find(int n) {
+		if(parent[n] == n) return n;
+		return parent[n] = find(parent[n]);
+	}
+	
+	private static void union(int u, int v ) {
+		u = find(u);
+		v = find(v);
+		if(u != v) parent[u] = v;
+	}
 }
