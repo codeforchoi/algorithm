a802e8808260379b5d9d09e3b2c5e94751400e12
SungHyun Choi <codeforchoi@gmail.com>
2026-08-07 15:00:06 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int answer = 0;
+
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(i, computers, visited);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    private static void dfs(int cur, int[][] computers, boolean[] vistied) {
+        vistied[cur] = true;
+
+        for(int i = 0; i < computers.length; i++) {
+            if(computers[cur][i] == 1 && !vistied[i]) {
+                dfs(i, computers, vistied);
+            }
+        }
+    }
+}
