26cdddbe3de011dccc24bb4e60122a8844432f3a
SungHyun Choi <codeforchoi@gmail.com>
2026-08-13 13:50:29 +0900

    [level 2] Title: 프로세스, Time: 0.79 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,42 +1,23 @@
 import java.util.*;
 
-class Solution {    
-    public class Process {
-        int index;
-        int priority;
-
-        public Process(int index, int priority) {
-            super();
-            this.index = index;
-            this.priority = priority;
-        }		
-    }
-
+class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Process> queue = new LinkedList<>(); // 실제 프로세스 순서 유지
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
-
-        for(int i = 0; i < priorities.length; i++) {
-            queue.offer(new Process(i, priorities[i]));
-            pq.offer(priorities[i]);
-        }
-
-        int order = 0;
-
-        while(!queue.isEmpty()) {
-            Process process = queue.poll();
-
-            if(process.priority == pq.peek()) {
-                pq.poll();
-                order++;
-
-                if(process.index == location) {
-                    return order;
-                }
-            } else {
-                queue.offer(process);
-            }
-        }
-        return -1;
+        int order = 1;
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for(int i = 0; i < priorities.length; i++) {			
+			pq.offer(priorities[i]);
+		}		
+		
+		while(!pq.isEmpty()) {
+			for(int i = 0; i < priorities.length; i++) {
+				if(priorities[i] == pq.peek()) {
+					pq.poll();
+					if(i == location) return order;
+					order++;
+				}
+			}
+		}
+		return -1;
     }
 }
