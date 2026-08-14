e7d142864d16a33aeaac8b780a42d6e12aa2d70f
SungHyun Choi <codeforchoi@gmail.com>
2026-08-14 16:08:10 +0900

    [level 2] Title: 주식가격, Time: 13.92 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 13.54 ms
+메모리: 74.8 MB, 시간: 13.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 07일 14:54:12
+2026년 08월 14일 16:08:08
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-		int[] result = new int[n];
-		
         Deque<Integer> stack = new ArrayDeque<>();
-        
-        for(int i = 0; i < n; i++) {
-        	// stack이 비어있지 않거나 가격이 떨어진 경우
-        	while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) { 
-        		int index = stack.pop();
-        		result[index] = i - index;
-        	}
-        	stack.push(i);
-        }
-        
-        while(!stack.isEmpty()) {
-        	int index = stack.pop();
-        	result[index] = (n - 1) - index;
-        }       
+		int n = prices.length;
+		int[] result = new int[prices.length];
+		
+		for(int i = 0; i < n; i++) {
+			while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+				int index = stack.pop();
+				result[index] = i - index;
+			}
+			stack.push(i);
+		}
+		
+		while(!stack.isEmpty()) {
+			int index = stack.pop();
+			result[index] = n - index - 1; 
+		}	
+		
         return result;
     }
 }
