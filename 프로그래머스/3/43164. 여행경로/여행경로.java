d37f272c4bc2e3b72f236765a74cf5a25621dc17
SungHyun Choi <codeforchoi@gmail.com>
2026-08-21 16:47:38 +0900

    [level 3] Title: 여행경로, Time: 1.41 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,10 +1,6 @@
 import java.util.*;
 
-class Solution {
-    private static boolean[] visited;
-	private static int n;
-	private static String[] result;
-    
+class Solution {   
     public String[] solution(String[][] tickets) {
         Map<String, PriorityQueue<String>> graph = new HashMap<>();
 		
