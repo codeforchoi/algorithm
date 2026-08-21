fcba3a982494a206f6a63ca76ccb8f7448355f52
SungHyun Choi <codeforchoi@gmail.com>
2026-08-21 15:42:24 +0900

    [level 3] Title: 여행경로, Time: 1.03 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     private static boolean[] visited;
-    private static int n;
-    private static List<String> result;
+	private static int n;
+	private static String[] result;
     
     public String[] solution(String[][] tickets) {
         Arrays.sort(tickets, (o1, o2) -> o1[1].compareTo(o2[1]));
@@ -11,11 +11,11 @@ class Solution {
 		n = tickets.length;
 		
 		visited = new boolean[n];
-		result = new ArrayList<>();
-		result.add("ICN");
-		
+		result = new String[n + 1];
+		result[0] = "ICN";
+        
 		dfs(tickets, "ICN", 0);        
-        return result.toArray(new String[0]);
+        return result;
     }
     
     private boolean dfs(String[][] tickets, String start, int depth) {
@@ -26,13 +26,12 @@ class Solution {
 		for(int i = 0; i < n; i++) {
 			if(!visited[i] && tickets[i][0].equals(start)) {
 				visited[i] = true;				
-				result.add(tickets[i][1]);
+				result[depth + 1] = tickets[i][1];
 				
 				if(dfs(tickets, tickets[i][1], depth + 1)) {
 					return true;
 				}
-				visited[i] = false; // 탐색이 끝나면 복구
-                result.remove(result.size() - 1); // 탐색이 끝나면 결과도 복구
+				visited[i] = false; // 탐색이 끝나면 복구				
 			}
 		}	
 		return false;
