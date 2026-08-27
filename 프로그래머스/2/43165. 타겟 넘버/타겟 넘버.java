a4b21b9eecb73f611ee7d7f008c4c57a8b0e4529
SungHyun Choi <codeforchoi@gmail.com>
2026-08-27 20:54:09 +0900

    [level 2] Title: 타겟 넘버, Time: 5.08 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int count = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return count;
+    }
+    
+    private void dfs(int[] numbers, int target, int depth, int sum) {
+        if(depth >= numbers.length) {
+            if(sum == target) count++;
+            return;
+        }
+
+        dfs(numbers, target, depth + 1, sum + numbers[depth]);
+        dfs(numbers, target, depth + 1, sum + numbers[depth] * (-1));
+    }
+}
