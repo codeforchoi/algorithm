e7b2c6b0676363d1202e6d583308a50ece256bf3
SungHyun Choi <codeforchoi@gmail.com>
2026-09-01 14:54:25 +0900

    [level 2] Title: 기능개발, Time: 2.82 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> q = new ArrayDeque<>();
+		List<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < progresses.length; i++) {
+			int remain = 100 - progresses[i];
+			int divide = speeds[i];
+			int result = remain / divide;
+			int day = (remain % divide == 0) ? result : result + 1;
+			q.offer(day);
+		}
+		
+		int maxPeriod = q.poll();
+		int count = 1;
+		while(!q.isEmpty()) {
+			int period = q.poll();
+			if(period <= maxPeriod) {
+				count++;
+			} else {
+                maxPeriod = period;
+				list.add(count);
+				count = 1;
+			}			
+		}
+		list.add(count);
+		return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
