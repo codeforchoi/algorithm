0d5b4498bd204774e0cdebf994ee2bdb430a4925
SungHyun Choi <codeforchoi@gmail.com>
2026-09-02 16:22:14 +0900

    [level 3] Title: 단어 변환, Time: 0.17 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,20 +1,10 @@
 import java.util.*;
 
 class Solution {
-    private class WordState {
-		String word;
-		int count;
-
-		public WordState(String word, int count) {
-			super();
-			this.word = word;
-			this.count = count;
-		}
-
-	}
+    private int minCount = Integer.MAX_VALUE;
     
     public int solution(String begin, String target, String[] words) {
-        // words에 target이 없는 경우 바로 0 반환
+       // words에 target이 없는 경우 바로 0 반환
 		boolean inWords = false;
 		for (String word : words) {
 			if (word.equals(target)) {
@@ -24,27 +14,26 @@ class Solution {
 		if (!inWords) return 0;
 
 		// words에 target이 있는 경우 변환 시도
-		Queue<WordState> q = new ArrayDeque<>();
-		boolean[] visited = new boolean[words.length];
 		
-		q.offer(new WordState(begin, 0));
-		while (!q.isEmpty()) {
-			WordState cur = q.poll();
-			
-			if(cur.word.equals(target)) {
-				return cur.count;
-			}
-			
-			for(int i = 0; i < words.length; i++) {
-				if(!visited[i] && canChange(cur.word, words[i])) {
-					visited[i] = true;
-					q.offer(new WordState(words[i], cur.count + 1));
-				}
-			}
+		boolean[] visited = new boolean[words.length];
+		dfs(words, visited, begin, target, 0);
+		return minCount;
+    }
+    
+    private void dfs(String[] words, boolean[] visited, String cur, String target, int count) {
+		if(cur.equals(target)) {
+			minCount = Math.min(minCount, count);
+			return;
 		}
 		
-		return 0;
-    }
+		for(int i = 0; i < words.length; i++) {
+			if(!visited[i] && canChange(cur, words[i])) {
+				visited[i] = true;
+				dfs(words, visited, words[i], target, count + 1);
+				visited[i] = false;
+			}			
+		}
+	}
     
     private boolean canChange(String cur, String next) {
 		int count = 0;
