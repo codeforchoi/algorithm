b98f821b34e4c5ab4063eb535dac28832950979f
SungHyun Choi <codeforchoi@gmail.com>
2026-09-18 16:41:23 +0900

    [level 2] Title: 모음 사전, Time: 0.32 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,15 +1,20 @@
+import java.util.*;
+
 class Solution {
-    private int[] number = {781, 156, 31, 6, 1};
+    private static int[] number = {781, 156, 31, 6, 1};
     
     public int solution(String word) {
         int answer = 0;
+		Map<Character, Integer> map = new HashMap<>();
+		map.put('A', 0);
+		map.put('E', 1);
+		map.put('I', 2);
+		map.put('O', 3);
+		map.put('U', 4);
+		
 		for(int i = 0; i < word.length(); i++) {
-			answer += toIndexOf(word.charAt(i)) * number[i] + 1;
+			answer += map.get(word.charAt(i)) * number[i] + 1;
 		}	
 		return answer;
     }
-    
-    private int toIndexOf(char ch) {
-		return "AEIOU".indexOf(ch);
-	}
 }
